3b86f8caff5918adabf3e4f86b6f3759ad084e7e
grapycolpis <grapysoda1951@gmail.com>
2022-06-13 08:15:13 -0700

    init.

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Requestment
+```bash
+sudo apt-get install qt5-default python3-opencv -y
+```
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp tuneGif.desktop ~/.local/share/applications
+cp tuneGif.desktop ~/Desktop/
